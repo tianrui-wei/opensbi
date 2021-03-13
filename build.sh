d06cb611f5619061fff4d65c2f948453715c9feb
Tianrui Wei <tianrui-wei@outlook.com>
2021-03-13 21:06:18 +0800

    added build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 #CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=fpga/openpiton FW_PAYLOAD_PATH=/home/eva/piton/piton-linux/arch/riscv/boot/Image
 #CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=fpga/openpiton FW_PAYLOAD_PATH=/home/eva/piton/piton-linux/arch/riscv/boot/Image
 #CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=fpga/openpiton FW_PAYLOAD_PATH=/home/eva/piton/piton-linux/arch/riscv/boot/Image FW_FDT_PATH=/home/eva/piton/uboot/arch/riscv/dts/openpiton-ariane.dtb
-CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=fpga/openpiton FW_PAYLOAD_PATH=/home/eva/piton/piton-linux/arch/riscv/boot/Image FW_FDT_PATH=/home/eva/piton/uboot/arch/riscv/dts/openpiton-ariane.dtb
+# CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=fpga/openpiton FW_PAYLOAD_PATH=/home/eva/piton/piton-linux/arch/riscv/boot/Image
+CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=fpga/openpiton FW_PAYLOAD_PATH=~/piton/uboot/u-boot-nodtb.bin
 #CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=fpga/openpiton FW_PAYLOAD_PATH=/home/eva/piton/piton-linux/arch/riscv/boot/Image FW_FDT_PATH=~/piton/dts/ariane.dtb
 #CROSS_COMPILE=/home/eva/piton/xpack-riscv-none-embed-gcc-10.1.0-1.1/bin/riscv-none-embed- make PLATFORM=generic FW_PAYLOAD_PATH=/home/eva/piton/piton-linux/arch/riscv/boot/Image FW_FDT_PATH=/home/eva/piton/uboot/arch/riscv/dts/openpiton-ariane.dtb
